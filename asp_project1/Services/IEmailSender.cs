18956d82e78530700637e01976d6f02ce331b670
Mehtab kiran <mehtabkiran66@gmail.com>
2017-11-15 01:44:28 +0500

    Add project files.

---
+++ asp_project1/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_project1.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
